032bf91a8c19bffe7652d8a8658927a8dc31cb3e
VMA9 <oooaliooo.mav@gmail.com>
2023-02-12 21:49:54 +0300

    first commit

---
+++ src/FixtureBuilder/Main.java
@@ -0,0 +1,9 @@
+package FixtureBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        Fixture fixture = new Fixture();
+        fixture.enterTeams();
+        fixture.setFixture();
+    }
+}
